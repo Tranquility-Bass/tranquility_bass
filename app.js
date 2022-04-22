67643a8c40e6157180335eafa09ae2907ba778e2
keramcgovern <keramcgovern@gmail.com>
2022-04-22 11:52:24 -0400

    Initalize project - coauthored with Patrick and Bella on VSlive

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const static = express.static(__dirname + '/public');
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+const handlebarsInstance = exphbs.create({
+  defaultLayout: 'main',
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    asJSON: (obj, spacing) => {
+      if (typeof spacing === 'number')
+        return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+      return new Handlebars.SafeString(JSON.stringify(obj));
+    }
+  }
+});
+
+app.use;
+app.use('/public', static);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.engine('handlebars', handlebarsInstance.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session({
+  name: 'AuthCookie',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/private', (req, res, next) => {
+  if (!req.session.user) {
+    return res.status(403).render('pages/login', {
+		title: "Login",
+		name: "Login",
+		error: "You are not logged in"
+	});
+  } else {
+    next();
+  }
+});
+
+app.use(async (req, res, next) => {
+	let date = new Date().toUTCString();
+	let method = req.method;
+	let url = req.originalUrl;
+	if (req.session.user) console.log("[" + date + "]: " + method + " " + url + " (Authenticated User)");
+	else console.log("[" + date +  "]: " +method + " "+ url + " (Non-Authenticated User)");
+	next();
+});
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
+
---
+++ data/users.js
@@ -0,0 +1,83 @@
+const { ObjectId } = require("mongodb");
+const mongoCollections = require("../config/mongoCollections")
+const users = mongoCollections.users;
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
+const validator = require("email-validator");
+
+
+function checkInput(val, type) {
+    if (!val) throw `${type} was not supplied.`;
+    if (val.length == 0) throw `${type} is too short.`;
+  
+    if (type == 'username') {
+        if (val.length < 4) throw `Username is too short`;
+        let validchars = "qwertyuiopasdfghjklzxcvbnm1234567890";
+        for (let i=0; i<val.length; i++){
+            if (!validchars.includes(val[i])) throw `Invalid character ${val[i]} supplied in username.`;
+        }
+        val = val.toLowerCase();
+    }
+    if (type == 'password') {
+        if (val.length < 6) throw `Password is too short`;
+        if (val.includes(' ')) throw `No spaces allowed in password.`;
+    }
+    if (type == "email") {
+        if (!validator.validate(val)) throw `Email is not a valid email address.`;
+    }
+    return val;
+  }
+
+async function createUser(username, password, email) {
+    if (arguments.length > 2) throw `Too many arguments passed.`
+    username = checkInput(username,'username');
+    password = checkInput(password,'password');
+    email = checkInput(email, "email");
+
+    const userCollection = await users();
+
+    const user = await userCollection.findOne({ username: username });
+    if (user) throw `Username already taken.`;
+
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    let newUser = {
+        username: username,
+        password: hash,
+        email: email,
+        discForumPosts: [],
+        reviewPosts: []
+    };
+
+    const insertInfo = await userCollection.insertOne(newUser);
+    if (insertInfo.insertedCount === 0) throw 'Could not add user';
+    
+    return {userInserted: true};
+}
+
+async function checkUser(username, password) {
+    if (arguments.length > 2) throw `Too many arguments passed.`
+    username = checkInput(username,'username');
+    password = checkInput(password,'password');
+
+    const userCollection = await users();
+
+    const user = await userCollection.findOne({ username: username });
+    if (!user) throw `Either the username or password is invalid`;
+
+    let compareMatch = false; 
+    try {
+        compareMatch = await bcrypt.compare(password, user.password);
+    } catch (e) {
+        //no op
+    }
+
+    if (compareMatch) {
+        return {authenticated: true}
+    }
+    else {
+        throw `Either the username or password is invalid`;
+    }
+}
+
+module.exports = {createUser,checkUser};
