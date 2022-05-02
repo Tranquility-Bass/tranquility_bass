3824bae1aaf5d9d4536799eeffceebedec3696b0
Isabella Cruz <bella.cruz42001@gmail.com>
2022-05-01 23:45:58 -0400

    login/signup code
    Added/updated routes and created handlebar pages for the login and signup feature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const topReviewedRoutes = require("./topReviewed");
 const addTopic = require("./addTopic");
+const login = require("./login");
+const signup = require("./signup");
 
 const constructorMethod = (app) => {
   app.use('/topReviewed', topReviewedRoutes);
   app.use('/private/addTopic', addTopic);
+  app.use('/login', login);
+  app.use('/signup', signup);
 
   app.use('*', (req, res) => {
     res.status(404).render('pages/error', {title: "Error", error:"Page Not Found"} );
