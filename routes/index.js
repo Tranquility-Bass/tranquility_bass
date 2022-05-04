603df24445e957679178aaac570a2111e2125593
Isabella Cruz <bella.cruz42001@gmail.com>
2022-05-04 13:15:57 -0400

    created homepage
    created homepage; connected to login/signup/user profile

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const topReviewedRoutes = require("./topReviewed");
 const addTopic = require("./addTopic");
 const search = require("./search");
 const account = require("./account");
+const home = require("./home");
 
 const constructorMethod = (app) => {
   app.use('/topReviewed', topReviewedRoutes);
   app.use('/private/addTopic', addTopic);
   app.use('/search', search);
   app.use('/account', account);
+  app.use('/', home);
 
   app.use('*', (req, res) => {
     res.status(404).render('pages/error', {title: "Error", error:"Page Not Found"} );
