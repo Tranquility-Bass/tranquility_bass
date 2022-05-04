ac62913c73ddda2591caf259ae725083b9a3e3f5
Patrick <pmcdonou@stevens.edu>
2022-05-04 13:45:46 -0400

    Making the reviews and discussions page

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const data = require('../data');
+const searchData = data.search;
+const { ObjectId } = require('mongodb');
 
 
 router.get('/', async (req, res) => {
@@ -20,6 +22,35 @@ router.get('/about', async (req, res) => {
         res.render('pages/about');
     }else{
         res.render('pages/about');
+    }
+});
+
+router.get('/all/:searchId', async (req, res) => {
+	try {
+		if (typeof req.params.searchId != 'string') throw 'Search ID must be a string';
+		req.params.searchId = req.params.searchId.trim();
+		if (req.params.searchId === "")throw 'Search ID must be a non empty string';
+		if (!ObjectId.isValid(req.params.searchId)) throw 'invalid search ID';
+	} catch (e) {
+		res.render('pages/error', {error: e, title: "Discussions and Reviews"});
+	}
+	try {
+      let discussions = await searchData.getDiscussions(req.params.searchId);
+	  let reviews = await searchData.getReviews(req.params.searchId);
+	  let emptyDiscussions = discussions.length == 0;
+      let emptyReviews = reviews.length == 0;
+
+      let val = {
+          title: "Discussions and Reviews",
+          discussions: discussions,
+          reviews: reviews,
+          emptyDiscussions : emptyDiscussions,
+          emptyReviews : emptyReviews
+      }
+
+      res.render('pages/search/all', val);
+    } catch (e) {
+      res.render('pages/error', {error: e, title: "Search Results"});
     }
 });
 
