cf4d71f2f2780a7dcc371a2b8a2972b13c8b2bab
keramcgovern <keramcgovern@gmail.com>
2022-05-08 21:01:15 -0400

    final changes

--- public/js/addTopic.js
+++ public/js/addTopic.js
@@ -8,19 +8,26 @@
       var newType = type.val();
       var actionUrl = addTopicForm.attr('action');
 
+      var errorP = $('#errorP');
       if (newType) {
-        var requestConfig = {
-            method: 'POST',
-            url: actionUrl,
-            data: addTopicForm.serialize()
-        };
+        if (newType != "artist" && newType != "album") {
+          errorP.text("Must select a value");
+        }
+        else {
+          errorP.text("");
+          var requestConfig = {
+              method: 'POST',
+              url: actionUrl,
+              data: addTopicForm.serialize()
+          };
 
-          $.ajax(requestConfig).then(function (responseMessage) {
-            console.log(responseMessage);
-            var newElement = $(responseMessage);
-            var nextStep = $('#nextStep');
-            nextStep.html(newElement);
-          });
+            $.ajax(requestConfig).then(function (responseMessage) {
+              console.log(responseMessage);
+              var newElement = $(responseMessage);
+              var nextStep = $('#nextStep');
+              nextStep.html(newElement);
+            });
+          }
         }
       }
     );
